fdeb795d863af38966622d051f2ed24dcb2b7f7c
leftfellow <dobrodush01@gmail.com>
2022-05-05 10:44:51 +0300

    добавил sonar

--- equation_test.py
+++ equation_test.py
@@ -1,27 +1,15 @@
-import unittest
 import main
+def test_discriminant_equal_zero():
+    assert main.solve_quadratic_equation(2, 4, 2) == -1
 
+def test_a_equal_zero():
+    assert main.solve_quadratic_equation(0, 6, 8) == -8/6
 
-class EquationTest(unittest.TestCase):
+def test_discriminant_higher_than_zero():
+    assert main.solve_quadratic_equation(2, 3, 1) == (-0.5, -1)
 
-    def setUp(self) -> None:
-        pass
+def test_discriminant_lower_than_zero():
+    assert main.solve_quadratic_equation(8, 4, 2) == 'Дискриминант меньше нуля'
 
-    def test_discriminant_equal_zero(self):
-        self.assertEqual(main.solve_quadratic_equation(2, 4, 2), -1)
-
-    def test_a_equal_zero(self):
-        self.assertEqual(main.solve_quadratic_equation(0, 6, 8), -8/6)
-
-    def test_discriminant_higher_than_zero(self):
-        self.assertEqual(main.solve_quadratic_equation(2, 3, 1), (-0.5, -1))
-
-    def test_discriminant_lower_than_zero(self):
-        self.assertEqual(main.solve_quadratic_equation(8, 4, 2), 'Дискриминант меньше нуля')
-
-    def test_zeroes(self):
-        self.assertEqual(main.solve_quadratic_equation(0, 0, 0), 'Деление на ноль')
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_zeroes():
+    assert main.solve_quadratic_equation(0, 0, 0) == 'Деление на ноль'
