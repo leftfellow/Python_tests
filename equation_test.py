28f84cb8e095850423f7a3679e5621fb9d1aa731
leftfellow <dobrodush01@gmail.com>
2022-05-12 11:01:45 +0300

    m

--- equation_test.py
+++ equation_test.py
@@ -1,15 +1,29 @@
 import main
-def test_discriminant_equal_zero():
-    assert main.solve_quadratic_equation(2, 4, 2) == -1
+import unittest
 
-def test_a_equal_zero():
-    assert main.solve_quadratic_equation(0, 6, 8) == -8/6
 
-def test_discriminant_higher_than_zero():
-    assert main.solve_quadratic_equation(2, 3, 1) == (-0.5, -1)
+class EquationTest(unittest.TestCase):
 
-def test_discriminant_lower_than_zero():
-    assert main.solve_quadratic_equation(8, 4, 2) == 'Дискриминант меньше нуля'
+    def setUp(self) -> None:
+        pass
 
-def test_zeroes():
-    assert main.solve_quadratic_equation(0, 0, 0) == 'Деление на ноль'
+    def test_discriminant_equal_zero(self):
+        self.assertEqual(main.solve_quadratic_equation(2, 4, 2), -1)
+
+    def test_a_equal_zero(self):
+        self.assertEqual(main.solve_quadratic_equation(0, 6, 8), -8/6)
+
+    def test_discriminant_higher_than_zero(self):
+        self.assertEqual(main.solve_quadratic_equation(2, 3, 1), (-0.5, -1))
+
+    def test_discriminant_lower_than_zero(self):
+        self.assertEqual(main.solve_quadratic_equation(
+            8, 4, 2), 'Дискриминант меньше нуля')
+
+    def test_zeroes(self):
+        self.assertEqual(main.solve_quadratic_equation(
+            0, 0, 0), 'Деление на ноль')
+
+
+if __name__ == '__main__':
+    unittest.main()
